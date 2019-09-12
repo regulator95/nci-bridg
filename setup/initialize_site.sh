a65390de1a5d8262ca2b96efaa38e8610aa677c4
genesis55 <kneislercp@nih.gov>
2019-09-12 13:02:03 -0400

    Added ldap

--- setup/initialize_site.sh
+++ setup/initialize_site.sh
@@ -1,28 +1,47 @@
 #!/bin/bash
-drushcmd = '/local/drupal/site/vendor/drush/drush/drush'
+
+cd /local/drupal/site
+
+#cp -p /tmp/services.yml /local/drupal/site/web/sites/default/
+#cp -p /tmp/settings.php /local/drupal/site/web/sites/default/
+#cp /tmp/.htaccess /local/drupal/site
+
+if [ -d "/local/drupal/site/docker/apache" ];then
+    echo "Adding addition apache config files"
+    cp /local/drupal/site/docker/apache/* /etc/httpd/conf.d
+fi
+echo ""
+echo "*** Compose install"
+echo ""
+
+composer install
+
 echo "*** Setting the website on docker after compose install"
 echo ""
 echo "*Setting up directory permissions"
+chown -R root:apache /local/drupal
 echo "chmod -R 775 /local/drupal/site/web/sites/default/files"
+echo "Recursive doesn't work for s3fs.  So skipping for now."
 chmod -R 775 /local/drupal/site/web/sites/default/files
 echo "chmod -R 664 /local/drupal/site/web/sites/default/s*"
 chmod -R 664 /local/drupal/site/web/sites/default/s*
 
-echo ""
-echo "*Add symbolic link to shared volume"
-echo "username = $username (i.e. the project name for docker)"
-echo "ln -s /mnt/s3fs/$username /local/drupal/site/web/sites/default/files/assets/s3fs"
-ln -s /mnt/s3fs /local/drupal/site/web/sites/default/files/assets/s3fs
+#echo ""
+#echo "*Add symbolic link to shared volume"
+#echo "username = $username (i.e. the project name for docker)"
+#echo "ln -s /mnt/s3fs/$username /local/drupal/site/web/sites/default/files/assets/s3fs"
+#ln -s /mnt/s3fs /local/drupal/site/web/sites/default/files/assets/s3fs
 #echo "ls -latr"
 #ls -latr
 
-echo "Import Config or Load Database if config doesn't work"
+#echo "Import Config"
 #echo "drush cim -y"
 #drush cim -y
+echo "Load Database"
 drush sql-cli < /local/drupal/site/database.sql
 
-echo "Adding server info"
+echo "Enable LDAP"
 
-/local/drupal/site/setup/enable_ldap.sh
+.setup/enable_ldap.sh
 
 
